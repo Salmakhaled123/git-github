ed0a1afd01aed2c5593fb61f93c2e8b5071031f7
Salmakhaled123 <toty5alouda@gmail.com>
2023-08-29 04:12:11 +0300

    add center title in app bar

--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,7 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(appBar: AppBar(title: const Text('salma')),);
+    return  Scaffold(appBar: AppBar(centerTitle: true),
+    );
   }
 }
